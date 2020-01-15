7fd068d5080d23f0e160bd961c31fd3e10aa0aed
leocavalcante <lc@leocavalcante.com>
2020-01-14 21:48:33 -0300

    :tada: Hello world on port 3333

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate actix_web;
+
+use actix_web::{App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello World"
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:3333")?
+        .run()
+        .await
 }
