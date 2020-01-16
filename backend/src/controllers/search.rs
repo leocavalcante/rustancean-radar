432fb510463201313b538c3f3f8b135994b4212b
leocavalcante <lc@leocavalcante.com>
2020-01-16 11:39:28 -0300

    Searching by location. We are done 🥂

--- backend/src/controllers/search.rs
+++ backend/src/controllers/search.rs
@@ -1,6 +1,7 @@
 use actix_web::{Error, get, HttpResponse};
 use actix_web::error::ErrorInternalServerError;
 use actix_web::web::Query;
+use diesel::expression::sql_literal::sql;
 use diesel::prelude::*;
 
 use crate::{establish_connection, schema};
@@ -15,9 +16,10 @@ pub async fn search(query: Query<SearchQuery>) -> Result<HttpResponse, Error> {
     let conn = establish_connection();
     let techs = utils::csv_to_vec(query.techs.to_string());
 
-    let devs = schema::devs::table.filter(
-        dsl::techs.overlaps_with(techs)
-    ).get_results::<Dev>(&conn)
+    let devs = schema::devs::table
+        .filter(dsl::techs.overlaps_with(techs))
+        .filter(sql(format!("2 * 3961 * asin(sqrt((sin(radians(({0} - lat) / 2))) ^ 2 + cos(radians(lat)) * cos(radians({0})) * (sin(radians(({1} - lng) / 2))) ^ 2)) < 10", query.lat, query.lng).as_str()))
+        .get_results::<Dev>(&conn)
         .map_err(ErrorInternalServerError)?;
 
     Ok(HttpResponse::Ok().json(devs))
