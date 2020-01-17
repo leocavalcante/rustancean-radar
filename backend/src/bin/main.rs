b52ae173b453c4836170b87c7cc787b56e06c85c
leocavalcante <lc@leocavalcante.com>
2020-01-17 02:05:14 -0300

    Add a logger in the backend

--- backend/src/bin/main.rs
+++ backend/src/bin/main.rs
@@ -1,11 +1,15 @@
 use actix_cors::Cors;
-use actix_web::{App, http::header, HttpServer};
+use actix_web::{App, http::header, HttpServer, middleware::Logger};
 
 use backend::controllers;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     HttpServer::new(|| App::new()
+        .wrap(Logger::default())
         .wrap(Cors::new()
             .allowed_origin("http://localhost:8000")
             .allowed_methods(vec!["GET", "POST"])
