b93de21d596c4cb993143ff9eeb5cf710f92d433
leocavalcante <lc@leocavalcante.com>
2020-01-17 19:47:47 -0300

    Almost there, the create/store route isn't working yet

--- backend/src/bin/main.rs
+++ backend/src/bin/main.rs
@@ -1,26 +1,26 @@
-use actix_cors::Cors;
-use actix_web::{App, http::header, HttpServer, middleware::Logger};
+use warp::Filter;
 
-use backend::controllers;
+use backend::application::SearchQuery;
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
+#[tokio::main]
+async fn main() {
+    let list_devs = warp::path!("devs")
+        .and(warp::get())
+        .and_then(backend::controllers::dev::index)
+        .clone();
 
-    HttpServer::new(|| App::new()
-        .wrap(Logger::default())
-        .wrap(Cors::new()
-            .allowed_origin("http://localhost:8000")
-            .allowed_methods(vec!["GET", "POST"])
-            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-            .allowed_header(header::CONTENT_TYPE)
-            .max_age(3600)
-            .finish())
-        .service(controllers::dev::store)
-        .service(controllers::dev::index)
-        .service(controllers::search::search))
-        .bind("127.0.0.1:3333")?
-        .run()
-        .await
+//    let create_dev = warp::path!("devs")
+//        .and(warp::post())
+//        .and(warp::body::json())
+//        .and_then(backend::controllers::dev::store)
+//        .clone();
+
+    let search_dev = warp::path!("search")
+        .and(warp::get())
+        .and(warp::query::<SearchQuery>())
+        .and_then(backend::controllers::search::search)
+        .clone();
+
+    let routes = list_devs.or(search_dev);
+    warp::serve(routes).run(([127, 0, 0, 1], 3333)).await;
 }
